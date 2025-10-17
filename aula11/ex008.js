6e5fe52559f0f40d3fde822d061b52db24aa5b3c
robsonsoaresid <robemsuave@gmail.com>
2025-10-17 20:13:10 -0300

    funcões

---
+++ aula11/ex008.js
@@ -0,0 +1,13 @@
+//var vel = 60.5
+//var vel = 12
+//console.log(`A velocidade do seu carro é ${vel}Km/h`)
+//console.log('Você ultrapassou velocidade permiteda. MULTADO!')
+//console.log('Dirija sempre usando o sinto de segurança')
+
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {  // Condoção simples
+    console.log('Você ultrapassou velocidade permiteda. MULTADO!')
+}
+console.log('Dirija sempre usando o sinto de segurança')
+
