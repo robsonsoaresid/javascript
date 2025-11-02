dbb241644d3c189a713f6099e8de1e503c544144
robsonsoaresid <robemsuave@gmail.com>
2025-11-02 15:11:29 -0300

    funções

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,35 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} adicionar`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor invalido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus() 
+}
